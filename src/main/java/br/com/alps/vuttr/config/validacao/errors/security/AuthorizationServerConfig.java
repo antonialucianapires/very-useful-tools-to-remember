0eeca944ae92a475d1f773ddc95594adb18c01a8
Antonia Luciana Pires <antonialucianapires@gmail.com>
2021-02-24 01:28:18 -0300

    config: configuração de token em memória

--- src/main/java/br/com/alps/vuttr/config/validacao/errors/security/AuthorizationServerConfig.java
+++ src/main/java/br/com/alps/vuttr/config/validacao/errors/security/AuthorizationServerConfig.java
@@ -14,10 +14,26 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
+    private static final Integer ACCESS_TOKEN_VALIDITY_IN_SECONDS = 60 * 60 * 24;
+    private static final Integer REFRESH_TOKEN_VALIDITY_IN_SECONDS = 60 * 60 * 24;
+    private static final String CLIENT = "client-id";
+    private static final String SECRET = "secret-id";
+
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
         security.tokenKeyAccess("permitAll()")
                 .checkTokenAccess("isAuthenticated()")
                 .allowFormAuthenticationForClients();
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient(CLIENT)
+                .secret(SECRET)
+                .authorizedGrantTypes("password", "authorization_code", "refresh_token", "implicit")
+                .scopes("read", "write", "trust")
+                .accessTokenValiditySeconds(ACCESS_TOKEN_VALIDITY_IN_SECONDS)
+                .refreshTokenValiditySeconds(REFRESH_TOKEN_VALIDITY_IN_SECONDS);
     }
 }
