a19f3c042d418fdabfac6cb31ddae0495ea17ab5
Antonia Luciana Pires <antonialucianapires@gmail.com>
2021-02-24 01:26:19 -0300

    config: configuração inicial do servidor de autenticação com Oauth2

---
+++ src/main/java/br/com/alps/vuttr/config/validacao/errors/security/WebSecurityConfig.java
@@ -0,0 +1,12 @@
+package br.com.alps.vuttr.config.validacao.errors.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated();
+    }
+}
